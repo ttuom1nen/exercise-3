02bdaa25fa25b8cfe2e5be1f4b6686fa8d431cae
ttuom1nen <ttuominen@outlook.com>
2019-06-01 21:55:52 +0300

    Form submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  onSearchSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: { q: term }
+    });
+
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
       <div className="ui container" style={{ "margin-top": "10px" }}>
-        <SearchBar />
+        <SearchBar onSearchSubmit={this.onSearchSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    //this.props.onSubmit(this.state.term);
+    this.props.onSearchSubmit(this.state.term);
   };
 
   render() {
