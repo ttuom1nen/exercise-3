3a3e2468b3e8ac9e812a18b11dff7e78ce4c387a
ttuom1nen <ttuominen@outlook.com>
2019-06-01 21:28:19 +0300

    Added public key and axios

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDEPhhlzvC-IC4q1S8vj_lC_gWg5YlxkaY";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
