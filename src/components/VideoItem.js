a8fdba5d29a21a81b2ef8fe9abef8960f9357b31
ttuom1nen <ttuominen@outlook.com>
2019-06-02 23:33:58 +0300

    Component key and img alt attr

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,14 @@ import "../css/VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
+  console.log(video);
   return (
     <div className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem video={video} />;
+    return <VideoItem key={video.id.videoId} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
