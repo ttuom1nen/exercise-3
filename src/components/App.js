98e92c68b5c195f1533e80f561e814297d8e3cbf
ttuom1nen <ttuominen@outlook.com>
2019-06-02 17:35:45 +0300

    Video list and video items added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [] };
+
   onSearchSubmit = async term => {
     const response = await youtube.get("/search", {
       params: { q: term }
     });
 
-    this.setState({ images: response.data.results });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
-      <div className="ui container" style={{ "margin-top": "10px" }}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
